83dc9d13d1b7c5a58487a683b3da98ec4274aebc
Danny Simfukwe <dannysimfukwe@gmail.com>
2018-05-04 11:18:20 +0200

    Feature: added a template(header section)

---
+++ views/app/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $data['data']['title'] ;?></title>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css">
+    
+    <style>
+        body { padding-top: 40px; }
+    </style>
+</head>
+<body>
