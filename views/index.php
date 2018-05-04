81aee76b17941710141695a40657ba8f4877fb7a
Danny Simfukwe <dannysimfukwe@gmail.com>
2018-05-04 11:17:37 +0200

    Refactor: Separated content sections

--- views/index.php
+++ views/index.php
@@ -1,31 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title><?php echo $data['title'] ;?></title>
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css">
-    
-    <style>
-        body { padding-top: 40px; }
-    </style>
-</head>
-<body>
 <section class="hero">
   <div class="hero-body">
     <div class="container">
       <h1 class="title">
-       <?php echo $data['title'] ?>
+       <?php echo $data['data']['title'] ?>
       </h1>
       <h2 class="subtitle">
-        <?php echo $data['msg'] ?>, This is the default page
+        <?php echo $data['data']['msg'] ?>, This is the default page
       </h2>
     </div>
   </div>
 </section>
-<div class="container is-fluid">
-  <div class="notification">
-   <p>Developed by <strong>Danny Simfukwe</strong></p>
-  </div>
-</div>
-</body>
-</html>
+
